ea38cc08473ef9eef178f9d66fd038da2e26cf70
Peter Mouland <peter.mouland@gmail.com>
2015-01-15 10:58:54 +0000

    make bin more agnostic + add js help in example file

--- component-structure/src/js/main.js
+++ component-structure/src/js/main.js
@@ -1,5 +1,10 @@
 // By default JS dependency is handled using browserify
 // please see 'GULP-TASKS.md#js' for more info
+//
+// You may need another component:
+// run : $ bower install bskyb-event --save
+// then add
+// var event = require('../../bower_components/bskyb-event/src/js/event');
 
 //example function and export
 function sum(args){
