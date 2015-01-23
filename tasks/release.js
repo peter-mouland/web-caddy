2aefa321c3c9f773dfb0d45796c64962e18ef3ec
Peter Mouland <peter.mouland@gmail.com>
2015-01-23 11:37:36 +0000

    add helpful info message during release

--- tasks/release.js
+++ tasks/release.js
@@ -12,6 +12,12 @@ function onError(err) {
     console.log(chalk.red(err.message));
     process.exit(1);
 }
+function info(msg) {
+    console.log(chalk.cyan(msg));
+}
+function onSuccess(msg) {
+    console.log(chalk.red(msg));
+}
 
 function gitRelease(version){
     return git.add(['.']).then(function() {
@@ -26,15 +32,17 @@ function gitRelease(version){
 }
 
 function versionBump(version, type){
+    info("\nBumping version ... \n");
     version = semver.inc(version, type);
     return bump('./*.json', {type: type}).then(function(){
-        return build.updateDocs({version: version})
+        return build.html(version)
     }).then(function(){
         return version;
     });
 }
 
 function ghPagesRelease(dir){
+    info("\nReleasing to gh-pages ... \n");
     dir = dir || './_site'
     return new Promise(function(resolve, reject){
         ghPages.publish(dir, function(err) {
@@ -46,9 +54,10 @@ function ghPagesRelease(dir){
 
 function awsRelease(version, name, config){
     if (!config.release){
-        console.log('AWS release set to false. skipping\nIf this is an error update the config and run `gulp release:aws`')
+        info('AWS release set to false. skipping\nIf this is an error update the config and run `gulp release:aws`')
         return
     }
+    info("\nReleasing to AWS ... \n");
     var s3 = aws.setup(config)
     return file.read('./_site/**/*.*').then(function(files){
         if (!files.length) onError({message: 'No files found to release to AWS\n' + glob})
@@ -62,11 +71,11 @@ function awsRelease(version, name, config){
 
 function all(version, name, config, type){
     return versionBump(version, type).then(function(version){
-        return Promise.all([
-            gitRelease(version),
-            ghPagesRelease(),
-            awsRelease(version, name, config)
-        ]);
+        return gitRelease(version);
+    }).then(function(){
+        return ghPagesRelease();
+    }).then(function(){
+       return awsRelease(version, name, config)
     });
 }
 
