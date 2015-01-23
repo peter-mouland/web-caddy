58b86e3e7ea024f51149692b116370dddc44d5b7
Peter Mouland <peter.mouland@gmail.com>
2015-01-23 17:25:17 +0000

    rename in gulp file

--- component-structure/gulpfile.js
+++ component-structure/gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var componentHelper = require('gulp-component-helper')(gulp);
+var componentHelper = require('component-helper')(gulp);
 var paths = componentHelper.paths;
 var runSequence = require('run-sequence');
 
