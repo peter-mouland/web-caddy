39263b121365249568c8d1d6255d190a93c6beb7
Peter Mouland <peter.mouland@gmail.com>
2015-02-06 16:46:55 +0000

    correct scripts directory in main.requirejs.js

--- component-structure/src/scripts/main.requirejs.js
+++ component-structure/src/scripts/main.requirejs.js
@@ -1,7 +1,7 @@
 var local = {}; local['{{ component }}'] = require('./{{ component }}');
 
 if (typeof window.define === "function" && window.define.amd) {
-    define('bower_components/bskyb-{{ component }}/dist/js/{{ component }}.requirejs', [], function() {
+    define('bower_components/bskyb-{{ component }}/dist/scripts/{{ component }}.requirejs', [], function() {
         'use strict';
         return local['{{ component }}'];
     });
