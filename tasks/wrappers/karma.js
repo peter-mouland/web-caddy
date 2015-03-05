8b7c3096c310b4df832fae5d1653caf12fead8fc
Peter Mouland <peter.mouland@gmail.com>
2015-03-05 12:15:19 +0000

    fix test

--- tasks/wrappers/karma.js
+++ tasks/wrappers/karma.js
@@ -51,7 +51,7 @@ Karma.prototype.coverage = function(){
         log.info(['To view coverage results please run',
             ' * $ component serve test/coverage/phantomjs/'].join('\n'));
         err && reject('Test Coverage FAILED');
-        !err && resolve('Test Coverage SUCCESS.');
+        !err && resolve('Test Coverage SUCCESS');
     });
 };
 
