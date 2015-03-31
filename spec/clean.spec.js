283c8f5f7699177b92dccff32156df49059623cb
Peter Mouland <peter.mouland@gmail.com>
2015-03-31 09:09:04 +0100

    add empty specs for more realistic code coverage report

---
+++ spec/clean.spec.js
@@ -0,0 +1,13 @@
+var Promise = require('es6-promise').Promise;
+var helper = require('../tasks/utils/config-helper');
+var log = require('../tasks/utils/log');
+var fs = require('../tasks/utils/fs');
+var Clean = require('../tasks/clean');
+
+describe("clean", function() {
+
+    beforeEach(function(){
+
+    });
+
+});
